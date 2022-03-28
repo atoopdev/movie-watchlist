d6fc65c124aea264531dc7b9ed1c7916f4e53d63
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-03-28 14:54:52 -0700

    getMovieInfo()
    -get movie imdb use for second fetch to query for extended data. save data to array - moviesData.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // api key  b86c75a0
 
-movies = []
+let moviesData = []
 
 // ---------------------------- grab form data -------------------------------
 
@@ -10,7 +10,9 @@ document.getElementById("movie-title-search").addEventListener("submit", event=>
     const ourFormData = new FormData(event.target)
     const movieTitle = ourFormData.get("movieTitle")
     // console.log("movieTitle: ", movieTitle)
-    getMovies(movieTitle)
+    getMoviesList(movieTitle)
+    console.log("Finished moviesData: ", moviesData)
+    // setTimeout(outputMovies, 3000)
 })
 
 // -----------------------------------------------------------------------------
@@ -21,18 +23,46 @@ document.getElementById("movie-title-search").addEventListener("submit", event=>
 
 // -------------------------- look up movie title ------------------------------
 
-function getMovies(searchTerm){
+function getMoviesList(searchTerm){
+    
     // start with hard coded title
-    fetch(`http://www.omdbapi.com/?s=${searchTerm}&apikey=b86c75a0`, {method: "GET"})
+    fetch(`http://www.omdbapi.com/?s=${searchTerm}&apikey=b86c75a0&plot=full`, {method: "GET"})
     .then(response => response.json())
     .then(data=>{
         // console.log("Movie data: ", data)
         // console.log("title 0: ", data.Search[0])
         for(let i=0;i<data.Search.length;i++){
-            movies.push(data.Search[i])
+            // moviesIMDB.push(data.Search[i].imdbID)
+           getMovieInfo(data.Search[i].imdbID)
         }
-        // console.log("Movies array: ", movies)
     })
+   
+}
+
+function getMovieInfo(movie){
+    let movieExtData = []
+fetch(`http://www.omdbapi.com/?i=${movie}&apikey=b86c75a0`, {method: "GET"})
+.then(response => response.json())
+.then(data =>{
+    console.log("Movie data from getMovieInfo: ", data)
+    moviesData.push(data)
+    // console.log("MoviesData", moviesData)
     
+})
+}
+
+function outputMovies(){
+    let moviesHTML = ""
+    for(let i=0;i<movies.length;i++){
+
+        // console.log(movies[i].Title)
+        // moviesHTML += `
+        // <div>
+        // <h2>Title: ${movies[i].Title}</h2>
+        // <img src="${movies[i].Poster}" alt = "Poster of ${movies[i].Title}"/>
+        // </div>`
+    }
+    console.log("moviesHTML: ", moviesHTML)
+    document.getElementById("search-results").innerHTML=moviesHTML
 }
 
