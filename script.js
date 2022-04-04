b7da537ce76b4b4c0ebb5a9bae1cb468c6dd2492
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-04-04 09:25:12 -0700

    add + icon to watchlist button
    set alignment

--- script.js
+++ script.js
@@ -82,9 +82,9 @@ function outputMovieHTML(arr){
         <img class="movie-poster" src="${arr[i].Poster}" alt = "Poster of ${arr[i].Title}"/>
         <div class="movie-summary">
         <p class="movie-title">${arr[i].Title} <span class="movie-rating">⭐️ ${arr[i].Ratings[0].Value}</span></p>
-        <p class="movie-details">${arr[i].Runtime} ${arr[i].Genre} 
+        <p class="movie-details">${arr[i].Runtime} ${arr[i].Genre} <button class="btn addtowatchlist"><i class="material-icons">add_circle</i> Watchlist</button> </p>
         <p class="movieID">${arr[i].imdbID}</p>
-        <button class="btn addtowatchlist">+ Watchlist</button></p>
+        
         <p class="movie-plot">${arr[i].Plot}</p>
         </div>
         </div>`
